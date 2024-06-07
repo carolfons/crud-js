18b49960c1d3cb55fa4d24c184638d52d10dce4a
carolfons <carolsantosfonseca@outlook.com>
2024-06-07 16:32:38 -0300

    input validation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,31 @@
-function validateField(){
-    const inputEmail = document.getElementById('email').value;
-    if(!email){
-        document.querySelector('.forgot-password')
-    }
+function validateFields(){
+    const emailValid = isEmailValid();
+    const passwordValid = isPasswordValid();
+
+    //botao de recuperar senha fica desabilitado se o email for invalido
+    document.getElementById('recover-password-button').disabled = !emailValid;
+
+    //botão de login fica desabilitado se o email e a senha forem invalidos
+    document.getElementById('login-btn').disabled = !emailValid || !passwordValid;
+
 }
+
+function isEmailValid(){
+    const email = document.getElementById('email').value;
+    if(!email){
+        return false;
+    }
+    return validateEmail(email)
+}
+
+function isPasswordValid(){
+    const password = document.getElementById('password').value;
+    if(!password){
+        return false;
+    }
+    return true;
+}
+
+function validateEmail (email) {
+    return /\S+@\S+\.\S+/.test(email)
+  }
